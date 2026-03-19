c222bbbb1af85ade932e98a3d8258d2012af831c
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-19 17:16:25 -0500

    change completed data type

--- src/main/java/com/sergio/taskmanager/Task.java
+++ src/main/java/com/sergio/taskmanager/Task.java
@@ -1,5 +1,5 @@
 package com.sergio.taskmanager;
 
-record Task(Long id, String title, String description, Boolean completed) {
+record Task(Long id, String title, String description, boolean completed) {
     
 }
