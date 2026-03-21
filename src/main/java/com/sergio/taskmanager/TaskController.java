051414e5f30870d6b252d2f76a0221b047cfab9e
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-20 19:40:42 -0500

    feat: add GET task endpoint

---
+++ src/main/java/com/sergio/taskmanager/TaskController.java
@@ -0,0 +1,19 @@
+package com.sergio.taskmanager;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<Task> findById(@PathVariable Long requestedId) {
+        Task task = new Task(99L, "Test Task", "This is a test task.", false);
+        return ResponseEntity.ok(task);
+    }
+    
+}
