1844e3ad7678c9c26cebb2c413a124434e5ddaf0
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-20 19:38:45 -0500

    test: add shouldReturnATask test

--- src/test/java/com/sergio/taskmanager/TaskmanagerApplicationTests.java
+++ src/test/java/com/sergio/taskmanager/TaskmanagerApplicationTests.java
@@ -1,13 +1,41 @@
 package com.sergio.taskmanager;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TaskmanagerApplicationTests {
 
+	@Autowired
+	TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
-	}
+	void shouldReturnATaskWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/tasks/99", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+		Number id = documentContext.read("$.id");
+		assertThat(id.intValue()).isEqualTo(99);
+
+		String title = documentContext.read("$.title");
+		assertThat(title).isEqualTo("Test Task");
+
+		String description = documentContext.read("$.description");
+		assertThat(description).isEqualTo("This is a test task.");
+
+		boolean completed = documentContext.read("$.completed");
+		assertThat(completed).isFalse();
+	}	
 
 }
