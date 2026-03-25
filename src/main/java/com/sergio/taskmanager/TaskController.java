f9b869ac0d3def1983a10783c59d8d1af9f2b705
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-25 17:32:30 -0500

    feat: add POST endpoint

--- src/main/java/com/sergio/taskmanager/TaskController.java
+++ src/main/java/com/sergio/taskmanager/TaskController.java
@@ -1,12 +1,16 @@
 package com.sergio.taskmanager;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/tasks")
@@ -26,6 +30,13 @@ public class TaskController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createTask(@RequestBody Task newTaskRequest, UriComponentsBuilder ucb) {
+        Task savedTask = taskRepository.save(newTaskRequest);
+        URI locationOfNewTask = ucb.path("/tasks/{id}").buildAndExpand(savedTask.id()).toUri();
+        return ResponseEntity.created(locationOfNewTask).build();
     }
     
 }
