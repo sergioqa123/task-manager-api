bb94fc67e832053495147047099ae56b4abc538f
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-27 18:20:12 -0500

    test: add more tasks to database

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO task (id, title, description, completed) VALUES (99, 'Test Task', 'This is a test task.', false);
+INSERT INTO task (id, title, description, completed) VALUES (100, 'Task 100', 'Description for Task 100', true);
+INSERT INTO task (id, title, description, completed) VALUES (101, 'Task 101', 'Description for Task 101', false);
