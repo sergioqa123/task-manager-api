a0f410c5ce902e435bcfa475a4a44f5739f25c63
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-27 18:56:03 -0500

    feat: add paginated task list endpoint

--- src/main/java/com/sergio/taskmanager/TaskController.java
+++ src/main/java/com/sergio/taskmanager/TaskController.java
@@ -1,15 +1,13 @@
 package com.sergio.taskmanager;
 
 import java.net.URI;
-import java.util.Optional;
+import java.util.*;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -40,8 +38,14 @@ class TaskController {
     }
 
     @GetMapping
-    private ResponseEntity<Iterable<Task>> findAll() {
-        return ResponseEntity.ok(taskRepository.findAll());
+    private ResponseEntity<List<Task>> findAll(Pageable pageable) {
+        Page<Task> page = taskRepository.findAll(
+            PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize()
+            )
+        );
+        return ResponseEntity.ok(page.getContent());
     }
     
 }
