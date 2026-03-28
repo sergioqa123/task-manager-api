abaed8c03c915e25940e3e189d964629077b2c4d
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-27 19:38:17 -0500

    feat: add default page sorting

--- src/main/java/com/sergio/taskmanager/TaskController.java
+++ src/main/java/com/sergio/taskmanager/TaskController.java
@@ -6,6 +6,7 @@ import java.util.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -43,7 +44,7 @@ class TaskController {
             PageRequest.of(
                 pageable.getPageNumber(),
                 pageable.getPageSize(),
-                pageable.getSort()
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "id"))
             )
         );
         return ResponseEntity.ok(page.getContent());
