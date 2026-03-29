8756954f385ecc53419c577523b1ae22b9ed3c40
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-29 18:47:02 -0500

    feat: add task owner field

--- src/main/java/com/sergio/taskmanager/Task.java
+++ src/main/java/com/sergio/taskmanager/Task.java
@@ -2,6 +2,6 @@ package com.sergio.taskmanager;
 
 import org.springframework.data.annotation.Id;
 
-record Task(@Id Long id, String title, String description, boolean completed) {
+record Task(@Id Long id, String title, String description, boolean completed, String owner) {
     
 }
