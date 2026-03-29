d05aeea47ab786b7c19ce7bd6ac0c43a7c07d8c3
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-29 18:44:27 -0500

    feat: add task owner field

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO task (id, title, description, completed) VALUES (99, 'Test Task', 'This is a test task.', false);
-INSERT INTO task (id, title, description, completed) VALUES (100, 'Task 100', 'Description for Task 100', true);
-INSERT INTO task (id, title, description, completed) VALUES (101, 'Task 101', 'Description for Task 101', false);
+INSERT INTO task (id, title, description, completed, owner) VALUES (99, 'Test Task', 'This is a test task.', false, 'sergio');
+INSERT INTO task (id, title, description, completed, owner) VALUES (100, 'Task 100', 'Description for Task 100', true, 'sergio');
+INSERT INTO task (id, title, description, completed, owner) VALUES (101, 'Task 101', 'Description for Task 101', false, 'sergio');
