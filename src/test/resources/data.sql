57a24281bec86a91ac883a2000e9a492efe87fd8
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-29 22:49:17 -0500

    test: add non-owner task fixture

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO task (id, title, description, completed, owner) VALUES (99, 'Test Task', 'This is a test task.', false, 'sergio');
 INSERT INTO task (id, title, description, completed, owner) VALUES (100, 'Task 100', 'Description for Task 100', true, 'sergio');
 INSERT INTO task (id, title, description, completed, owner) VALUES (101, 'Task 101', 'Description for Task 101', false, 'sergio');
+INSERT INTO task (id, title, description, completed, owner) VALUES (102, 'Task 102', 'Description for Task 102', true, 'david');
