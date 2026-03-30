87560870d686a9c12c90063cce13092a6bcea9e8
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-03-29 22:50:01 -0500

    feat: add owner-scoped repo queries

--- src/main/java/com/sergio/taskmanager/TaskRepository.java
+++ src/main/java/com/sergio/taskmanager/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.sergio.taskmanager;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 interface TaskRepository extends CrudRepository<Task, Long>, PagingAndSortingRepository<Task, Long> {
-    
+    Task findByIdAndOwner(Long id, String owner);
+    Page<Task> findByOwner(String owner, Pageable pageable);
 }
