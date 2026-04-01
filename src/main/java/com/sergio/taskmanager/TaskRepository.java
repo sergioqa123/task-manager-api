f7581ab5a7ce032611a893a65263fed588167c76
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-04-01 15:49:56 -0500

    feat: add owner exists check

--- src/main/java/com/sergio/taskmanager/TaskRepository.java
+++ src/main/java/com/sergio/taskmanager/TaskRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 interface TaskRepository extends CrudRepository<Task, Long>, PagingAndSortingRepository<Task, Long> {
     Task findByIdAndOwner(Long id, String owner);
+    boolean existsByIdAndOwner(Long id, String owner);
     Page<Task> findByOwner(String owner, Pageable pageable);
 }
